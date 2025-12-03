29cdf87006a33b2dbbd6f136720a6b6d1a1881b4
roflalex <C24319666@MyTUDublin.ie>
2025-12-03 20:49:29 +0000

    created dataclasses for tune and the abc metadata

--- manager.py
+++ manager.py
@@ -8,4 +8,25 @@ from dataclasses import dataclass
 
 logger = logging.getLogger('books')
 
+@dataclass 
+class BookMetadata:
+    path: str
+    prefix: str
+    index: int
+    name: str
 
+@dataclass
+class Tune:
+    #X:
+    reference_number: int
+    #T 
+    title: str
+    #M 
+    meter: str
+    #L
+    length: str
+    #R 
+    rhythm: str
+    #K 
+    key:str
+    
