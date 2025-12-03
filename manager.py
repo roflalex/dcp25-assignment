1d34a5fa314a14d2e5e9e49d9ab894aa7af6e1b4
roflalex <C24319666@MyTUDublin.ie>
2025-12-03 21:11:31 +0000

    added search method to BookManager for finding possible tunes in the directory

--- manager.py
+++ manager.py
@@ -38,5 +38,26 @@ class BookManager:
     
     def _load_possible_tunes(self)->list[str]:
         return glob.glob(self.pattern)
-    
-    
+    #find all possible tunes in the directory
+    def search(self)-> list[BookMetadata]:
+        paths = self._load_possible_tunes()
+        paths = [pathlib.Path(p) for p in paths]
+        #^ = start regex
+        #() = capture regex
+        #(.*) is the range for the regex
+        #testaalex
+        #[0] = testaalex
+        #[1] = test
+        #[2] = alex
+        #?: = non capturing group i.e. im looking for the / and the \ but i dont want to return it
+        #[0:9] = look for digits and match 
+        # this is bc the directory needs numbers 
+        # (.*) means accept any name of file
+        # finally look for file name ending in .abc
+        
+        pattern = r"^(.*)(?:/|\\)([0-9]+)(?:/|\\)(.*)\.abc$"
+        paths = [p for p in paths if p.is_file()]
+        #matches pattern to the path
+        paths = [re.match(pattern,str(p)) for p in paths]
+        # construct capturing groups 
+        return [BookMetadata(p[0], p[1], p[2], p[3]) for p in paths if p is not None]        
